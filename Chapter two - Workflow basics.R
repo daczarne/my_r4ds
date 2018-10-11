9276687473f36dc74dba62649816458044f3c7e1
daczarne <daniel.czarnievicz@gmail.com>
2018-10-11 20:00:04 -0300

    start chapter three

--- Chapter two - Workflow basics.R
+++ Chapter two - Workflow basics.R
@@ -4,6 +4,24 @@
 
 # Asignmet operator shortcut: Alt + - (alt key and minus sign)
 
+#### ----------------------------------- EXERCISES ---------------------------------- ####
+
+# 1)
+# Because there's a "spelling" mistake in the call
+
+# 2) 
+library(tidyverse)
+
+ggplot(data=mpg) +
+      geom_point(mapping=aes(x=displ, y=hwy))
+
+filter(mpg, cyl == 8)
+filter(diamonds, carat > 3)
+
+# 3) 
+# Alt+Shif+k displays keyboard shortcut quick reference
+
+#----------------------------------------------------------------------------------------#
 
 ################################
 #### FIN DE LA PROGRAMACIÓN ####
