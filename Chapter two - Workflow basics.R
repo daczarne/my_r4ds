2bdc419125c97c1717588d6f65c537df676dd6d7
daczarne <daniel.czarnievicz@gmail.com>
2018-10-11 19:22:43 -0300

    start

---
+++ Chapter two - Workflow basics.R
@@ -0,0 +1,10 @@
+######################################
+#### CHAPTER TWO: WORKFLOW BASICS ####
+######################################
+
+# Asignmet operator shortcut: Alt + - (alt key and minus sign)
+
+
+################################
+#### FIN DE LA PROGRAMACIÓN ####
+################################
