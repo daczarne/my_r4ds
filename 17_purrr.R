75479176576277bc880bab1692a480982c2c2670
Daniel Czarnievicz <daniel.czarnievicz@gmail.com>
2019-09-04 21:15:46 -0300

    exercises page 328

--- 17_purrr.R
+++ 17_purrr.R
@@ -2,19 +2,199 @@
 #### PURRR ####
 ###############
 
-
 # Introduction ------------------------------------------------------------
 
 library(tidyverse)
+library(nycflights13)
 
+# Imperative programming: for loops, while loops
+# Functional programming: functiona
 
 # For Loops ---------------------------------------------------------------
 
+# Every for loop has three components:
+#     1) output: allocate sufficient space for the output before starting the loop
+#     2) sequence: determines what to loop over
+#     3) body: the code that does the work
+
+# example: calculate the median of each variables in df
+df <- tibble(
+   a = rnorm(10),
+   b = rnorm(10),
+   c = rnorm(10),
+   d = rnorm(10)
+)
+
+output <- vector("double", ncol(df))   # 1. output
+for (i in seq_along(df)) {             # 2. sequence
+   output[[i]] <- median(df[[i]])      # 3. body
+}
+output
+
+#==========================# EXERCISES PG. 316 #==========================#
+# 1)
+# a. compute the mean of every column in mtcars
+output <- vector("double", ncol(mtcars))
+for (i in seq_along(mtcars)) {
+   output[[i]] <- mean(mtcars[[i]])
+}
+output
+
+# b. type of column in nyc flights13::flights
+output <- matrix(data = NA, nrow = ncol(nycflights13::flights), ncol = 2)
+colnames(output) <- c("variable", "clase")
+for (i in seq_along(nycflights13::flights)) {
+   output[i, "variable"] <- names(nycflights13::flights[i])
+   output[i, "clase"] <- typeof(nycflights13::flights[[i]])
+}
+output
+
+# c. number of unique values in each column of iris
+output <- matrix(data = NA, nrow = ncol(iris), ncol = 2)
+colnames(output) <- c("variable", "cant_vals_unicos")
+for (i in seq_along(iris)) {
+   output[i, "variable"] <- names(iris[i])
+   output[i, "cant_vals_unicos"] <- length(unique(iris[[i]]))
+}
+output
+
+# d. 10 random normals for mus
+mus <- c(-10, 0, 10, 100)
+output <- matrix(data = NA,
+                 nrow = length(mus),
+                 ncol = 10,
+                 dimnames = list(
+                    paste("mu =", mus)
+                 ))
+for (i in 1:dim(output)[1]) {
+   for (j in 1:dim(output)[2]) {
+      output[i, j] <- rnorm(n = 1, mean = mus[i])
+   }
+}
+output
+
+# 2)
+# caso a)
+out <- ""
+for (x in letters) {
+   out <- stringr::str_c(out, x)
+}
+out
+
+stringr::str_flatten(letters)
+
+# caso b)
+set.seed(1234)
+x <- sample(100)
+sd <- 0
+for (i in seq_along(x)) {
+   sd <- sd + (x[i] - mean(x)) ^ 2
+}
+sd <- sqrt(sd / (length(x) - 1))
+
+sd(x)
+
+# caso c)
+set.seed(1234)
+x <- runif(100)
+out <- vector("numeric", length(x))
+out[1] <- x[1]
+for (i in 2:length(x)) {
+   out[i] <- out[i - 1] + x[i]
+}
+out
+
+cumsum(x)
+
+# 3) combine functions and for loops
+# caso a) Alice the Camel
+animals <- c("Alice the Camel has one hump.",
+             "Ruby the Rabbit has two ears.",
+             "Sally the Sloth has three toes.",
+             "Felix the Fox has four legs.",
+             "Lilly the Ladybug has five spots.",
+             "Andy the Ant has six legs.",
+             "Larry the Lizard has seven stripes.",
+             "Sammy the Spider has eight legs.")
+for (i in seq_along(animals)) {
+   for (j in 1:3) {
+      print(
+         animals[i]
+      )
+   }
+   print(
+      paste(
+         "Go", 
+         stringr::word(animals[i], 1),
+         "go!"
+      )
+   )
+}
+
+#=========================================================================#
 
 
 # For Loop Variations -----------------------------------------------------
 
+# There are four variations on the basic theme of the for loop:
+#     1) Modifying an existing object, instead of creating a new object.
+#     2) Looping over names or values, instead of indices.
+#     3) Handling outputs of unknown length.
+#     4) Handling sequences of unknown length.
 
+## Modifying an existing object
+
+df <- tibble(
+   a = rnorm(10),
+   b = rnorm(10),
+   c = rnorm(10),
+   d = rnorm(10)
+)
+rescale01 <- function(x) {
+   rng <- range(x, na.rm = TRUE)
+   (x - rng[1]) / (rng[2] - rng[1])
+}
+
+for (i in seq_along(df)) {
+   df[[i]] <- rescale01(df[[i]])
+}
+
+## Looping patterns
+
+#  a. looping over indices: for (i in seq_along(x))
+#  b. looping over elements: for (i in x)
+#  c. looping over names: for (i in names(x))
+
+results <- vector("list", length(x))
+names(results) <- names(x)
+for (i in seq_along(x)) {
+   name <- names(x)[[i]]
+   value <- x[[i]]
+}
+
+## Unknown output lenght
+
+# Increase the output size (not recommended)
+means <- c(0, 1, 2)
+output <- double()
+for (i in seq_along(means)) {
+   n <- sample(100, 1)
+   output <- c(output, rnorm(n, means[[i]]))
+}
+str(output)
+
+# Recomendarion: save them to a list a then combine
+out <- vector("list", length(means))
+for (i in seq_along(means)) {
+   n <- sample(100, 1)
+   out[[i]] <- rnorm(n, means[[i]])
+}
+str(out)
+str(unlist(out))
+
+## Unknown sequence length
+
+# use while loop
 
 # For Loops versus functionals --------------------------------------------
 
@@ -36,10 +216,10 @@ library(tidyverse)
 # The type of the vector is determined by the suffix to the map function.
 
 df <- tibble(
-      a = rnorm(10),
-      b = rnorm(10),
-      c = rnorm(10),
-      d = rnorm(10)
+   a = rnorm(10),
+   b = rnorm(10),
+   c = rnorm(10),
+   d = rnorm(10)
 )
 
 map_dbl(.x = df, .f = mean)
@@ -59,30 +239,74 @@ map_int(.x = z, .f = length)
 ### Shortcuts
 
 models <- mtcars %>%
-      split(.$cyl) %>%
-      map(function(df) lm(mpg ~ wt, data = df))
+   split(.$cyl) %>%
+   map(function(df) lm(mpg ~ wt, data = df))
 
 # Use one sided formulae to create functions
 models <- mtcars %>%
-      split(.$cyl) %>%
-      map(~lm(mpg ~ wt, data = .))
+   split(.$cyl) %>%
+   map(~lm(mpg ~ wt, data = .))
 
 models %>%
-      map(summary) %>%
-      map_dbl(~.$r.squared)
+   map(summary) %>%
+   map_dbl(~.$r.squared)
 
 models %>%
-      map(summary) %>%
-      map_dbl("r.squared")
+   map(summary) %>%
+   map_dbl("r.squared")
 
 x <- list(list(1, 2, 3), list(4, 5, 6), list(7, 8, 9))
 x %>% map_dbl(2)
 
-#------- EXERCISES -------#
+#------- EXERCISES PAGE 328 -------#
+# 1) use map_*
+# a)
+mtcars %>% map_dbl(mean)
+mtcars %>% map_dbl(median)
+mtcars %>% map_dbl(quantile, 0.50)
+mtcars %>% map_dbl(var)
+mtcars %>% map_dbl(sd)
+mtcars %>% map_dbl(IQR)
+mtcars %>% map_dbl(quantile, 0.25)
+mtcars %>% map_dbl(quantile, 0.75)
 
+# b)
+flights %>% map_chr(typeof)
 
+# c)
+iris %>% as_tibble() %>% map_dbl(function(x) length(unique(x)))
 
-#-------------------------#
+# d)
+set.seed(1234)
+c(-10, 0, 10, 100) %>%
+   map(function(x) rnorm(10, mean = x)) %>%
+   unlist() %>%
+   matrix(nrow = 4, byrow = TRUE)
+
+# 2)
+iris %>% 
+   as_tibble() %>% 
+   map(is.factor) %>% 
+   flatten_lgl()
+
+# 3) for each run of the runif it's passing the corresponding element of the numeric vector as the first
+# argument, ie number of simulations
+map(1:5, runif)
+# can use it as other arguments defining a fucntion that does so
+map(1:5, .f = function(x) runif(1, min = x, max = x + 1))
+
+# 4)
+# generates a list of length 5 each element containing a numeric vector of length 5
+map(-2:2, rnorm, n = 5)
+# generates an error since input is a numeric vector of length 5. Output needs to be either a list as
+# before or a single element (double vector of length 1)
+map_dbl(-2:2, rnorm, n = 5)
+
+# 5) what's the expected output of this call?? what is it mapping over??
+map(x, function(df) lm(mpg ~ wt, data = df))
+mtcars %>% map(~lm(mpg ~ wt, data = .))
+
+#---------------------------------#
 
 # Dealing with failure ----------------------------------------------------
 
